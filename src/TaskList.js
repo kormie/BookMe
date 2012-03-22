6d9bc6eb06f45ca0e880d37b3862293050735bcf
David <kormie@gmail.com>
2012-03-22 07:41:08 -0500

    Move return statements out of for block
    This was causing a really annoying bug. It meant that only the first
    element checked would return true and everything else was false. I also
    removed some more stupid semi colons.

--- src/TaskList.js
+++ src/TaskList.js
@@ -18,8 +18,8 @@ taskList.prototype.completedTasks = function(){
 	completed = []
 	for (var i=0; i < this.list.length; i++) {
 		if (this.list[i].completed == false) {
-			completed.push(this.list[i]);
-		};
-		return completed;
-	};
+			completed.push(this.list[i])
+		}
+	}
+	return completed
 }
